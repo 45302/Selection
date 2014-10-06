70443da5f08916cd3cdd0b8833d33e5cfa31f646
Alexander Allan <45302@student.longroad.ac.uk>
2014-10-06 12:15:17 +0100

    Commmited

--- Selection_Task 2-3.py
+++ Selection_Task 2-3.py
@@ -4,8 +4,9 @@
 
 print("This program will tell you how much you will be getting payed this week.")
 WorkHours = float(input("Please enter the amount of hours you have worked this week: "))
-if WorkHours >60 and WorkHours <0:
-    print("There is an error working this out")
-HourlyWage = float(input("Please enter your hourly wage: £"))
-Payment = (WorkHours * HourlyWage)
-print("This week, you will be getting payed £{0}.".format(Payment))
+if WorkHours < 0 or WorkHours > 60:
+    print("You work either too many hours, or not enough hours to program.")
+else:
+    HourlyWage = float(input("Please enter your hourly wage: £"))
+    Payment = (WorkHours * HourlyWage)
+    print("This week, you will be getting payed £{0}.".format(Payment))
